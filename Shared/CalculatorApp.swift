695b41e217bcd8ee9d49e270153b8c1db1c262ea
Job Cerfontain <job.m.cerfontain@gmail.com>
2022-04-11 13:27:36 +0200

    Initial Commit

---
+++ Shared/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Shared
+//
+//  Created by Job Cerfontain on 11/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
